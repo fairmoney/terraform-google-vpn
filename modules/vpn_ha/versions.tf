64f6d62e2206b2f9c442b711ba64ccc6a4b9dc3e
aglshiv <156825172+aglshiv@users.noreply.github.com>
2025-01-20 16:33:10 +0530

    update google provider version to latest (#1)
    Co-authored-by: Shivam Agrawal <shivam@Shivams-MacBook-Pro.local>

--- modules/vpn_ha/versions.tf
+++ modules/vpn_ha/versions.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 5.7, < 6"
+      version = "~> 6.16.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 5.7, < 6"
+      version = "~> 6.16.0"
     }
     random = {
       source  = "hashicorp/random"
